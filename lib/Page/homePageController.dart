6920170f7cd6c92c458c5479b5ce314f6eaa6c42
pmahala <paragmahala@gmail.com>
2021-08-16 18:54:05 +0530

    Fixed look on cards

--- lib/Page/homePageController.dart
+++ lib/Page/homePageController.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class HomePageController extends GetxController {
-  var pageController = PageController().obs;
+  var pageController = PageController(viewportFraction: 0.8).obs;
   var currentIndex = 0.obs;
 
   void updateCurrentIndex(int index) {
