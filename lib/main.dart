a50e48a8698386184a3434668dda4c573b479728
pmahala <paragmahala@gmail.com>
2021-08-18 10:16:34 +0530

    renamed A file myHomePage to tutorial.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bouncing_cards/Page/myHomePage.dart';
+import 'package:bouncing_cards/Page/tutorial.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
